ae2913da93ba71a60f3c1c7fa8177ecfb59ebb0a
Justin Yoo <justin.yoojh@gmail.com>
2023-03-13 10:51:42 +0900

    Add ffmpeg tool

--- src/HelperApp/Triggers/FfmpegHttpTrigger.cs
+++ src/HelperApp/Triggers/FfmpegHttpTrigger.cs
@@ -11,6 +11,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
+using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
@@ -32,15 +33,41 @@ namespace DevRelKR.OpenAIConnector.HelperApp.Triggers
         [FunctionName(nameof(FfmpegHttpTrigger.ConvertAsync))]
         [OpenApiOperation(operationId: "Convert", tags: new[] { "converter" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "x-functions-key", In = OpenApiSecurityLocationType.Header)]
+        [OpenApiParameter(name: "input", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The input file format")]
+        [OpenApiParameter(name: "output", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The output file format")]
         [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(object), Description = "The input file data")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(object), Description = "The output file data")]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Description = "Either request header or body is invalid")]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Description = "Resource not found")]
         public async Task<IActionResult> ConvertAsync(
-            [HttpTrigger(AuthorizationLevel.Function, "POST", Route = "convert/{source}/{target}")] HttpRequest req,
-            string source, string target)
+            [HttpTrigger(AuthorizationLevel.Function, "POST", Route = "convert/{input}/{output}")] HttpRequest req,
+            string input, string output,
+            ExecutionContext context)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
+            if (input.IsNullOrWhiteSpace())
+            {
+                return new NotFoundResult();
+            }
+            if (output.IsNullOrWhiteSpace())
+            {
+                return new NotFoundResult();
+            }
+
+            using var reader = new StreamReader(req.Body);
+            var body = await reader.ReadToEndAsync();
+            if (body.IsNullOrWhiteSpace())
+            {
+                return new BadRequestResult();
+            }
+
             string name = req.Query["name"];
+
+            var fncappdir = context.FunctionAppDirectory;
+            var fncdir = context.FunctionDirectory;
+            var ffmpeg = Path.Combine(fncappdir, "Tools/ffmpeg.exe");
+            var exists = File.Exists(ffmpeg);
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
